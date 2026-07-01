62e1588f61ad5ace4bd6b1ae9355ba049f7a7fd9
Rafael Lima <rafaelima1250@gmail.com>
2026-07-01 00:03:59 -0300

    Add PID class and initialize motors and sensors

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from openrdk import CommsRuntime
 from odometria import Odometry
+import time
+
+velocidade_base = 0
+KP = 0
+KI = 0
+KD = 0
 
 runtime = CommsRuntime(
     auto_start=True,
@@ -7,6 +13,18 @@ runtime = CommsRuntime(
     enable_webview_updates=True,
 )
 
-devices = runtime.list_devices()
-motor = runtime.traction(serial_number)
-sensor = runtime.line_sensor(serial_number)
+motor_right = runtime.traction(serial_number)
+motor_left = runtime.traction(serial_number)
+line_sensor = runtime.line_sensor(serial_number)
+
+class PID:
+    def __init__ (self, kp, ki, kd):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.last_error = None
+        self.integral = 0
+        self.last_time = None
+    
+    def limitar_velocidade(valor):
+        return max(-100, min(100, valor))
