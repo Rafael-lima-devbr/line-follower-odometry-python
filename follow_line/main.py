456c6bc9a7ba3320dce4935f255ec28677183e5f
Rafael Lima <rafaelima1250@gmail.com>
2026-07-23 22:21:54 -0300

    Remove line sensor settings from main.py
    Removed line sensor configuration for track type and thresholds.

--- follow_line/main.py
+++ follow_line/main.py
@@ -16,10 +16,6 @@ motor_r = runtime.traction("98:3D:AE:43:50:50")
 motor_l = runtime.traction("10:20:BA:AA:E7:28")
 line_sensor = runtime.line_sensor("10:20:BA:AC:F4:B0")
 
-line_sensor.set_track_type("dark")
-line_sensor.set_digital_threshold(0.5)
-line_sensor.set_detect_threshold(0.2)
-
 motores = Motors(right = motor_r, left = motor_l)
 
 pid = PID()
